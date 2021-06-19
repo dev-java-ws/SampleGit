29d0ee681bd11c09cc9300c79731b06c3c2e41fb
devia <devia@DESKTOP-QRUFRFS>
2021-06-19 12:54:31 -0500

    Adding Comment to file

--- ff-spring-boot-config/src/main/java/io/friendlyfoods/ffspringbootconfig/GreetingController.java
+++ ff-spring-boot-config/src/main/java/io/friendlyfoods/ffspringbootconfig/GreetingController.java
@@ -10,6 +10,10 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author devia
+ *
+ */
 @RestController
 public class GreetingController {
 
