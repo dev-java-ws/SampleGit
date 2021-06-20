a1371a0aff7ed533f6c7c7dcbf4cb07076a548a5
devia <devia@DESKTOP-QRUFRFS>
2021-06-20 14:25:42 -0500

    Checking in project source code for springboot project with GIT and Docker integration
    Signed-off-by: devia <devia@DESKTOP-QRUFRFS>

---
+++ git-spring-boot/src/main/java/io/friendlyfoods/gitspringboot/GitSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.friendlyfoods.gitspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpringBootApplication.class, args);
+	}
+
+}
---
+++ git-spring-boot/src/main/java/io/friendlyfoods/gitspringboot/GitSpringBootSample.java
@@ -0,0 +1,17 @@
+package io.friendlyfoods.gitspringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitSpringBootSample {
+
+	public GitSpringBootSample() {
+	}
+	
+	@RequestMapping("/messages")
+	public String getMessageFromClass() {
+		return "<html><body><font color=green><b>Hello</b> from GitSpringBootSample</font></body></html>";
+	}
+
+}
